ba102faf6b5238b485d5594d0016782931df4484
ericrock461 <ericrock461@ed.amdsb.ca>
2018-09-17 14:47:49 -0400

    just a few added things

--- Constellation Greeting Card/Form1.Designer.cs
+++ Constellation Greeting Card/Form1.Designer.cs
@@ -28,24 +28,55 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
+            this.title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.title.Font = new System.Drawing.Font("Microsoft JhengHei", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(21, 47);
+            this.title.Name = "title";
+            this.title.Size = new System.Drawing.Size(280, 49);
+            this.title.TabIndex = 0;
+            this.title.Text = "The Big Dipper";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Liberation Sans", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(17, 186);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(201, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "click anywhere to open";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = global::Constellation_Greeting_Card.Properties.Resources.big_dipper_ladle;
-            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.ClientSize = new System.Drawing.Size(802, 418);
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(546, 392);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
             this.Name = "Form1";
             this.Text = "Greeting Card";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.Click += new System.EventHandler(this.Form1_Click);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
     }
 }
 
