d35668e34a5de25ab8db057a0cda7999fbae3f7c
ericrock461 <ericrock461@ed.amdsb.ca>
2018-09-20 14:52:57 -0400

    added more stars, and a paragraph of text at the end.

--- Constellation Greeting Card/Form1.cs
+++ Constellation Greeting Card/Form1.cs
@@ -37,31 +37,74 @@ namespace Constellation_Greeting_Card
             Graphics g = this.CreateGraphics();
             Pen drawPen = new Pen(Color.Aquamarine, 4);
             SolidBrush drawBrush = new SolidBrush(Color.White);
-            Font drawFont = new Font("Arial", 16, FontStyle.Bold);
+            Font drawFont = new Font("Arial", 8, FontStyle.Bold);
 
             g.Clear(Color.Black);
+            Thread.Sleep(1000);
 
             //individually draw every 'star' and keep them on-screen
             //beginning with unrelated stars, which have less time between appearances   
-
             g.FillEllipse(drawBrush, 134, 143, 3, 3);
             Thread.Sleep(1000);
             g.FillEllipse(drawBrush, 367, 84, 3, 3);
             Thread.Sleep(900);
             g.FillEllipse(drawBrush, 256, 231, 3, 3);
-            Thread.Sleep(800);
+            Thread.Sleep(900);
             g.FillEllipse(drawBrush, 140, 313, 3, 3);
-            Thread.Sleep(600);
+            Thread.Sleep(700);
             g.FillEllipse(drawBrush, 427, 354, 3, 3);
-            Thread.Sleep(300);
+            Thread.Sleep(500);
             g.FillEllipse(drawBrush, 118, 262, 3, 3);
+            Thread.Sleep(400);
+            g.FillEllipse(drawBrush, 83, 422, 3, 3);
+            Thread.Sleep(250);
+            g.FillEllipse(drawBrush, 158, 302, 3, 3);
             Thread.Sleep(100);
-            g.FillEllipse(drawBrush, 118, 262, 3, 3);
+            g.FillEllipse(drawBrush, 425, 124, 3, 3);
             Thread.Sleep(100);
-            g.FillEllipse(drawBrush, 118, 262, 3, 3);
+            g.FillEllipse(drawBrush, 235, 170, 3, 3);
             Thread.Sleep(100);
-            //remember to add 40 degrees to all future little stars
-
+            g.FillEllipse(drawBrush, 435, 286, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 171, 345, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 57, 182, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 410, 335, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 73, 176, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 51, 291, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 235, 170, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 166, 84, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 443, 265, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 297, 148, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 366, 85, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 226, 245, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 68, 125, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 256, 331, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 247, 372, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 331, 308, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 360, 346, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 293, 137, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 244, 102, 3, 3);
+            Thread.Sleep(100);
+            g.FillEllipse(drawBrush, 144, 205, 3, 3);         
+            Thread.Sleep(1000);
+            //the last small star has a longer delay
 
             //now the constellation stars, which have more time between appearances
             g.FillEllipse(drawBrush, 78, 222, 8, 8);
@@ -81,8 +124,7 @@ namespace Constellation_Greeting_Card
 
             //make lines between the stars to draw the constellation/asterism
             g.DrawLine(drawPen, 82, 226, 158, 189);
-            Thread.Sleep(700);
-            //ADD SOUNDS
+            Thread.Sleep(700);           
             g.DrawLine(drawPen, 158, 189, 216, 195);
             Thread.Sleep(700);
             g.DrawLine(drawPen, 216, 195, 288, 198);
@@ -94,9 +136,13 @@ namespace Constellation_Greeting_Card
             g.DrawLine(drawPen, 422, 209, 405, 139);
             Thread.Sleep(700);
             g.DrawLine(drawPen, 405, 139, 288, 198);
-            Thread.Sleep(700);
+            Thread.Sleep(700);  
 
-            g.DrawString("The Big Dipp...er is blah blah blah", drawFont, drawBrush, 50, 40);
+            g.DrawString("The Big Dipper, or the Plough is a large asterism consisting of seven bright stars" +
+                "\nof the constellation Ursa Major; six of them are of second magnitude and one, " +
+                "\nMegrez (δ), of third magnitude (Megrez is the star in the middle). " +
+                "\nFour define a 'bowl' and three define a 'handle'. " +
+                "\nIt is recognized as a distinct grouping in many cultures.", drawFont, drawBrush, 50, 30);
             //stay on here, don't go away and do weird stuff pls.
             
 
